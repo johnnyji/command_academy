2220bec7667d3163632d47b711705486b049fbfd
Johnny Ji <johnny.ji@live.ca>
2015-04-23 15:58:23 -0700

    configured seed data

--- app/actions.rb
+++ app/actions.rb
@@ -43,38 +43,11 @@ get '/admin/index' do
 end
 
 get '/begin' do
-  # @challenge = Challenge.all.order('level DESC').limit(1).first
   erb :challenges
 end
 
 get '/challenges' do
-  challenges = Challenge.order(level: :asc)
-  # challenges = [
-  #   {
-  #     instructions: "This is the instructions for challenge 1",
-  #     success: "SUCCESS Challenge 1",
-  #     fail: "FAIL Challenge 1",
-  #     level: 0,
-  #     answer: "pwd",
-  #     console_text: "/Users/owner/desktop"
-  #   },
-  #   {
-  #     instructions: "This is the instructions for challenge 2",
-  #     success: "SUCCESS Challenge 2",
-  #     fail: "FAIL Challenge 2",
-  #     level: 1,
-  #     answer: "ls",
-  #     console_text: "photos documents public \n\n vacation work personal"
-  #   },
-  #   {
-  #     instructions: "This is the instructions for challenge 3",
-  #     success: "SUCCESS Challenge 3",
-  #     fail: "FAIL Challenge 3",
-  #     level: 2,
-  #     answer: "mkdir project",
-  #     console_text: ""
-  #   },
-  # ]
+  challenges = Challenge.all.order(level: :asc)
   content_type :json
     challenges.to_json
 end
