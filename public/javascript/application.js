80a4515616786d723578469d66f6df319d305108
Johnny Ji <johnny.ji@live.ca>
2015-04-23 00:30:00 -0700

    added next challenge function

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -4,23 +4,34 @@ $(function() {
   $.getJSON('/challenges').then(function(data) {
     var challenges = data;
     var consoleInput = $('.console-input-field');
+    var consoleInputWrapper = $('.console-input-wrapper');
     var currentLevel = 0;
+
+    var nextChallenge = function(level) {
+      var currentChallenge = challenges[level];
+      $('.challenge-instructions').text(currentChallenge.instructions);
+      $('.challenge-title').text(currentChallenge.answer);
+      $('.challenge-answer').text(currentChallenge.answer);
+    };
+
     consoleInput.keydown(function(e) {
       if (e.which === 13) {
         var userAnswer = consoleInput.val();
         consoleInput.val('');
         var userAnswerDiv = $('<div>');
         userAnswerDiv.text('> ' + userAnswer);
-        userAnswerDiv.insertBefore(consoleInput);
+        userAnswerDiv.insertBefore(consoleInputWrapper);
         if (userAnswer === challenges[currentLevel].answer) {
           // if the user's input matches the answer of the current challenge
           var successMessageDiv = $('<div>');
           successMessageDiv.addClass('success').text(challenges[currentLevel].success);
-          successMessageDiv.insertBefore(consoleInput);
+          successMessageDiv.insertBefore(consoleInputWrapper);
+          currentLevel ++;
+          nextChallenge(currentLevel);
         } else {
           var failureMessageDiv = $('<div>');
           failureMessageDiv.addClass('fail').text(challenges[currentLevel].fail);
-          failureMessageDiv.insertBefore(consoleInput);
+          failureMessageDiv.insertBefore(consoleInputWrapper);
         }
       }
 
